7dc3bfaaee003c797d8566c33314b0325d560f9d
Watcher06 <rajani.ekta@gmail.com>
2017-06-11 15:09:09 +0400

    Create run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -46,13 +46,40 @@ View(alldata)
 
 select_mean_std<-grep('mean|std',names(alldata))
 sub_data<-alldata[,c(select_mean_std)]
+
 #the sub_data has columns with meanFreq as well, not clear if assignment requires these columns as well.
-#therefore remove meanFreq
+#therefore removed meanFreq
 View(sub_data)
 dim(sub_data)
 rmmeanFreq<-grep('meanFreq',names(sub_data))
 newsubdata<-sub_data[,-(rmmeanFreq)]
 dim(newsubdata)
+
+#Part3 - Uses descriptive activity names to name the activities in the data set (Used the dataset with meansFreq)
+activitylabel<-read.table('./UCI HAR Dataset/activity_labels.txt', header = FALSE)
+activitylabel<-as.character(activitylabel[,2])
+sub1<-alldata[,c(1:2)]
+newsubdata1<-cbind(sub1,sub_data)
+newsubdata1$activity<-activitylabel[newsubdata1$activity]
+
+#Part4 - Appropriately labels the data set with descriptive variable names.
+newnames<-names(newsubdata1)
+newnames<-gsub("[(][)]", "",newnames)
+newnames<-gsub("^t", "TimeDomain_",newnames)
+newnames<-gsub("^f", "FrequencyDomain_",newnames)
+newnames<-gsub("Acc", "Accelerometer",newnames)
+newnames<-gsub("Gyro", "Gyroscope",newnames)
+newnames<-gsub("Mag", "Magnitude",newnames)
+newnames<-gsub("-mean-", "_Mean_",newnames)
+newnames<-gsub("-std-", "_StandardDeviation_",newnames)
+newnames<-gsub("-", "_",newnames)
+newnames<-gsub("_std","_StandardDeviation",newnames)
+View(newnames)
+names(newsubdata1)<-newnames
+
+#Part5 - From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+tidydata<-aggregate(newsubdata1[,3:81],by=list(activity=newsubdata1$activity,subject=newsubdata1$subject),FUN=mean)
+write.table(x=tidydata,file="tidydata.txt",row.names=FALSE)
 
 
 
